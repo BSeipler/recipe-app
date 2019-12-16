699607183a0a134269a6fdb5a71fa0d2875a7d9e
blakeseipler <bseipler@yahoo.com>
2019-12-15 20:47:18 -0500

    Updates

---
+++ precache-manifest.3022208af6528028549aefdb6e23bd6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666c163f3b42fa224a0078e94adba076",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "b3b60d36692e29f2ee6d",
+    "url": "/recipe-app/static/css/main.0da6e519.chunk.css"
+  },
+  {
+    "revision": "791e736d4afe18d70241",
+    "url": "/recipe-app/static/js/2.6ab5e6ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-app/static/js/2.6ab5e6ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3b60d36692e29f2ee6d",
+    "url": "/recipe-app/static/js/main.7d041a89.chunk.js"
+  },
+  {
+    "revision": "608075b4c9971711eaef",
+    "url": "/recipe-app/static/js/runtime-main.029354cf.js"
+  }
+]);
